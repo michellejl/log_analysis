e1a966b588892ecb23e11a23dece22dbdb190570
Michelle JL <michelle@michellejl.com>
2017-08-15 18:51:46 -0700

    fixing to fit PEP8 style standards

--- loganalysisdb.py
+++ loganalysisdb.py
@@ -6,7 +6,8 @@ DBNAME = "news"
 
 
 def run_query(query):
-    """Connects to the database, runs the query passed to it, and returns the results"""
+    """Connects to the database, runs the query passed to it,
+    and returns the results"""
     db = psycopg2.connect('dbname=' + DBNAME)
     c = db.cursor()
     c.execute(query)
@@ -36,7 +37,10 @@ def get_top_articles():
     print('\nTOP THREE ARTICLES BY PAGE VIEWS:')
     count = 1
     for i in results:
-        print('(' + str(count) + ') "' + i[0] + '" with ' + str(i[1]) + " views")
+        number = '(' + str(count) + ') "'
+        title = i[0]
+        views = '" with ' + str(i[1]) + " views"
+        print(number + title + views)
         count += 1
 
 
@@ -45,14 +49,14 @@ def get_top_article_authors():
 
     # Build Query String
     query = """
-        SELECT authors.name, COUNT(*) AS num 
-        FROM authors 
+        SELECT authors.name, COUNT(*) AS num
+        FROM authors
         JOIN articles
         ON authors.id = articles.author
         JOIN log
         ON log.path like concat('/article/%', articles.slug)
         GROUP BY authors.name
-        ORDER BY num DESC 
+        ORDER BY num DESC
         LIMIT 3;
     """
 
@@ -72,20 +76,21 @@ def get_days_with_errors():
 
     # Build Query String
     query = """
-        SELECT total.day, ROUND(((errors.error_requests*1.0) / total.requests), 3) AS percent
+        SELECT total.day,
+          ROUND(((errors.error_requests*1.0) / total.requests), 3) AS percent
         FROM (
-          SELECT date_trunc('day', time) "day", count(*) AS error_requests 
-          FROM log 
-          WHERE status LIKE '404%' 
+          SELECT date_trunc('day', time) "day", count(*) AS error_requests
+          FROM log
+          WHERE status LIKE '404%'
           GROUP BY day
         ) AS errors
         JOIN (
-          SELECT date_trunc('day', time) "day", count(*) AS requests 
-          FROM log 
+          SELECT date_trunc('day', time) "day", count(*) AS requests
+          FROM log
           GROUP BY day
-          ) AS total 
-        ON total.day = errors.day 
-        WHERE (ROUND(((errors.error_requests*1.0) / total.requests), 3) > 0.01) 
+          ) AS total
+        ON total.day = errors.day
+        WHERE (ROUND(((errors.error_requests*1.0) / total.requests), 3) > 0.01)
         ORDER BY percent DESC;
     """
 
@@ -95,7 +100,9 @@ def get_days_with_errors():
     # Print Results
     print('\nDAYS WITH MORE THAN 1% ERRORS:')
     for i in results:
-        print(i[0].strftime('%B %d, %Y') + " -- " + str(round(i[1]*100, 1)) + "%" + " errors")
+        date = i[0].strftime('%B %d, %Y')
+        errors = str(round(i[1]*100, 1)) + "%" + " errors"
+        print(date + " -- " + errors)
 
 print('Calculating Results...\n')
 get_top_articles()
